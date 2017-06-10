45f5e23aaf2f357dab12be5e35f67896a52af714
jerry <jerry.h.zhan@gmail.com>
2017-06-10 00:51:13 -0400

    used gon to pass data from rails to front-end javascript

--- app/assets/javascripts/puzzles.js
+++ app/assets/javascripts/puzzles.js
@@ -1,6 +1,7 @@
 //= require chessboard
 $(document).ready(function() {
 
+
 var onDrop1 = function(source, target, piece, newPos, oldPos, orientation) {
   console.log("Board 1:")
   console.log("Source: " + source);
@@ -26,21 +27,21 @@ var onDrop2 = function(source, target, piece, newPos, oldPos, orientation) {
 var cfg1 = {
   orientation: 'white',
   draggable: true,
-  position: 'r1bq1bkr/ppp3pp/2n1p3/3Bp3/8/5Q2/PPPP1PPP/RNB1K2R',
+  position: gon.puzzle.board1_FEN,
   onDrop: onDrop1,
   pieceTheme: '/assets/chesspieces/wikipedia/{piece}.png',
   sparePieces: true,
-  spareCounts: { 'wK':0, 'wQ':0, 'wR':0, 'wB':0, 'wN':0, 'wP':1, 'bK':0, 'bQ':0, 'bR':0, 'bB':0, 'bN':0, 'bP':1 },
+  spareCounts: gon.puzzle.board1_spare,
 };
 
 var cfg2 = {
   orientation: 'black',
   draggable: true,
-  position: 'rnbqkb1r/ppp2ppp/5n2/3p4/2PP4/5N2/PP3PPP/RNBQKB1R',
+  position: gon.puzzle.board2_FEN,
   onDrop: onDrop2,
   pieceTheme: '/assets/chesspieces/wikipedia/{piece}.png',
   sparePieces: true,
-  spareCounts: { 'wK':0, 'wQ':0, 'wR':0, 'wB':0, 'wN':1, 'wP':1, 'bK':0, 'bQ':0, 'bR':0, 'bB':0, 'bN':1, 'bP':1 },
+  spareCounts: gon.puzzle.board2_spare,
 }
 
 var board1 = ChessBoard('board1', cfg1);
--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,2 +1,8 @@
 class PuzzlesController < ApplicationController
+
+def show
+  gon.puzzle = Puzzle.find(params[:id])
+  @puzzle = gon.puzzle
+end
+
 end
